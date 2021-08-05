9c6ec3fdb0cf992768ee94d527e4c54250d90970
unknown <d.dogan@etu.edu.tr>
2021-08-05 23:36:07 +0300

    Card component and main component added

---
+++ solitaire-game/src/components/Card.vue
@@ -0,0 +1,58 @@
+<template>
+  <v-card
+    class="main-card d-flex align-stretch mb-6"
+    color="grey lighten-2"
+    flat
+    tile
+  >
+    <div class="row">
+      <div class="col-sm-3 card-header-footer">
+        <div class="rank">K</div>
+        <div class="rank small"></div>
+      </div>
+      <div class="col-sm-6">
+        <v-img
+          class="white--text align-end"
+          height="130px"
+          weight="80px"
+          src="./../assets/maca-buyuk.webp"
+        >
+        </v-img>
+      </div>
+      <div class="col-sm-3 card-header-footer">
+        <div>K</div>
+        <div>
+          <v-img
+            class="white--text align-end"
+            height="10px"
+            weight="10px"
+            src="./../assets/maca-min.png"
+          >
+          </v-img>
+        </div>
+      </div>
+    </div>
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: "Card",
+};
+</script>
+
+<style>
+.main-card {
+  width: 134px;
+  height: 210px;
+}
+.card-header-footer {
+  height: 40px;
+}
+.card-middle {
+  height: 130px;
+}
+.small{
+    background-image: url('./../assets/maca-min.png')
+}
+</style>
---
+++ solitaire-game/src/components/MainPage.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="green-table">
+      <card/>
+  </div>
+</template>
+
+<script>
+import Card from './Card.vue';
+
+export default {
+  components: { Card },
+  name: "MainPage",
+};
+</script>
+
+<style>
+.green-table {
+  display: flex;
+  justify-content: space-evenly;
+  height: 100vh;
+  width: 100vw;
+  border-width: initial;
+  border-style: none;
+  border-color: initial;
+  border-image: initial;
+  padding: 0px;
+  background-image: radial-gradient(
+    rgba(57, 172, 57, 0.725),
+    rgb(0, 116, 0),
+    rgb(0, 100, 0)
+  );
+}
+</style>
